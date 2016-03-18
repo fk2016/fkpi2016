d927ec8fd13a43a099e611c76fd4fc8e4216874a
fk2016 <flickgames.contato@gmail.com>
2016-03-18 12:53:39 -0300

    segundo commit

--- cadastrando.php
+++ cadastrando.php
@@ -2,35 +2,16 @@
 
 // recebe os dados do html
 
-$nome=$_POST['nome'];
-$sobrenome=$_POST['sobre'];
-$email=$_POST['email'];
-$senha=md5($_POST['senha']);
-$tipo=$_POST['tipo'];
-$sexo=$_POST['sexo'];
-$pais=$_POST['pais'];
-$termo=$_POST['termo'];
+
 
 // dados para conexão no banco
 
-$conexao="localhost";
-$user="root";
-$senha="";
+	$host= 'localhost';
+	$banco='fkdb';
+	$user= 'root';
+	$pass='';
+	
+	$conexao=mysql_connect($host,$user,$pass) or die (mysql_error()) ;
+	mysql_select_db($banco) or die (mysql_error());
 
-$connect=mysql_connect('$conexao','$user','$senha');
-$db = mysql_select_db('fkdb');
-$query_select = "SELECT nome FROM registro WHERE nome = '$nome'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['nome'];
-
-if (!$connect){
-
-	die('Não foi possivel conectar no banco de dados:'.mysql_error());
-
-} else{
-
-    echo"Conectado com sucesso";
-    mysql_close($connect);
-}
 ?>
